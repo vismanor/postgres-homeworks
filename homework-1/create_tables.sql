56749cb2f4058cfaff84915cd5123f01b3f84786
vismanor <147613028+vismanor@users.noreply.github.com>
2024-01-22 23:50:38 +0300

    Update create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(10) NOT NULL,
+	last_name varchar(10) NOT NULL,
+	title varchar(30) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id	 varchar(5) UNIQUE PRIMARY KEY,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(5) REFERENCES customers(customer_id),
+	employee_id	int REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_city varchar(100) NOT NULL
+);
